9ab6a1f55390531e93e09f5bfa40cb301844f04c
Alisher Khalykbayev <alisher12398@gmail.com>
2019-05-31 17:17:59 +0600

    Add auth, models

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField('Name', max_length=60)
+    price = models.IntegerField('Price')
+    quantity = models.IntegerField('Quantity', default=0)
+
+    def __str__(self):
+        return '{}: {}'.format(self.id, self.name)
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'name': self.name
+        }
+
+class UserProductManager(models.Manager):
+    def for_user(self, user):
+        return self.filter(user=user)
+
+
+class UserProduct(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    count = models.IntegerField('Count', default=0)
+
+    objects = UserProductManager()
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
+    'rest_framework.authtoken',
     'api',
     'auth_'
 ]
@@ -77,6 +78,15 @@ TEMPLATES = [
         },
     },
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    )
+}
+
 
 WSGI_APPLICATION = 'backend.wsgi.application'
 
